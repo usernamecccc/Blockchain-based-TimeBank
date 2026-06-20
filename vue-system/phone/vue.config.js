181eded283a483354c3bb84e661248978175654f
Neko65536 <kangfx918@163.com>
2026-06-20 21:47:13 +0800

    修复无法登录

--- vue-system/phone/vue.config.js
+++ vue-system/phone/vue.config.js
@@ -1,8 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+﻿const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
   transpileDependencies: true,
   devServer: {
-    port: 7001
+    port: 7001,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 })
